80fe7b9d6b57c07935674643673574d08807f27d
AlexisJ731 <AlexisJacobson731@gmail.com>
2024-01-30 11:52:43 -0600

    Added the controller and view files for the communication page.

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/CommunicateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MidStateShuttleService.Controllers
+{
+    public class CommunicateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
