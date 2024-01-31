c69d19aa2c0d48e01d250417bbdac650e40b1ade
hphoomyint <lynne4791@gmail.com>
2024-01-30 20:14:09 -0600

    RV-18 Created date and time picker for the Registeration form

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Register.js
@@ -0,0 +1,20 @@
+﻿
+//date and time picker
+document.addEventListener("DOMContentLoaded", function () {
+    var dateInput = document.getElementById('dateInput');
+    var timeInput = document.getElementById('timeInput');
+
+    dateInput.addEventListener('focus', function () {
+        this.type = 'date';
+    });
+    dateInput.addEventListener('blur', function () {
+        this.type = 'text';
+    });
+
+    timeInput.addEventListener('focus', function () {
+        this.type = 'time';
+    });
+    timeInput.addEventListener('blur', function () {
+        this.type = 'text';
+    });
+});
