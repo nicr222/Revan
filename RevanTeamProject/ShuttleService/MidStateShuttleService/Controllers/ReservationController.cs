f24378ce9856a7cc36fe22f776bda0651a300d6b
Nathan Brock <17134831@mstc.edu>
2024-02-01 02:37:29 -0600

    Added Reservation Controller and CheckIn view

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/ReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MidStateShuttleService.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
