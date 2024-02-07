8ee4739da03cd9b678f52cc5596845e07c4e99dc
Nicolas Ross <15972995@mstc.edu>
2024-02-07 13:28:57 -0600

    RV-28 Added Classes to Match some of the ERD. Can be switched as needed.

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/BusRoute.cs
@@ -0,0 +1,17 @@
+﻿namespace MidStateShuttleService.Models
+{
+    public class BusRoute
+    {
+        // Primary Key
+        public int BusRouteID { get; set; }
+
+        // Foreign Keys to Bus and Route tables
+        public int BusID { get; set; }
+        public Bus Bus { get; set; }
+
+        public int RouteID { get; set; }
+
+        public Routes Route { get; set; }
+    }
+
+}
