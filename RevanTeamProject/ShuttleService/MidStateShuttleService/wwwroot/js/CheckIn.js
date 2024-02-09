1c1f6f95293a5ca651f5424ccca31e3077db7e0a
Nathan Brock <17134831@mstc.edu>
2024-02-08 18:09:51 -0600

    finished view fields

--- RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/CheckIn.js
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/CheckIn.js
@@ -1,6 +1,50 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    $("input").on( "blur", function () {
+    $("#studentID").on("blur", function () {
+        // Regular Expresion for only 8 didgets
+        var regex = /^\d{8}$/;
+
+        if (regex.test($(this).val())) {
+            $(this).removeClass("is-invalid").addClass("is-valid");
+        } else {
+            $(this).removeClass("is-valid").addClass("is-invalid");
+        }
+    });
+
+    $("#firstName").on("blur", function () {
+        // Regular Expresion for only 8 didgets
+        var regex = /^\d{8}$/;
+
+        if (regex.test($(this).val())) {
+            $(this).removeClass("is-invalid").addClass("is-valid");
+        } else {
+            $(this).removeClass("is-valid").addClass("is-invalid");
+        }
+    });
+
+    $("#lastName").on("blur", function () {
+        // Regular Expresion for only 8 didgets
+        var regex = /^\d{8}$/;
+
+        if (regex.test($(this).val())) {
+            $(this).removeClass("is-invalid").addClass("is-valid");
+        } else {
+            $(this).removeClass("is-valid").addClass("is-invalid");
+        }
+    });
+
+    $("#startLocation").on("blur", function () {
+        // Regular Expresion for only 8 didgets
+        var regex = /^\d{8}$/;
+
+        if (regex.test($(this).val())) {
+            $(this).removeClass("is-invalid").addClass("is-valid");
+        } else {
+            $(this).removeClass("is-valid").addClass("is-invalid");
+        }
+    });
+
+    $("#endLocation").on("blur", function () {
         // Regular Expresion for only 8 didgets
         var regex = /^\d{8}$/;
 
