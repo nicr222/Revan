9be25a691e532cba90dfb4304cfe7650126014da
Nicolas Ross <15972995@mstc.edu>
2024-02-13 16:14:40 -0600

    RV-24 Begging to roll our own logger. Added class and started adding content

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/LogEvents.cs
@@ -0,0 +1,34 @@
+﻿namespace MidStateShuttleService.Models
+{
+    public class LogEvents
+    {
+        public static void LogToFile(string title, string logMessage, IWebHostEnvironment env)
+        {
+            bool exists = Directory.Exists(env.WebRootPath + "\\" + "LogFolder");
+            if (!exists)
+            {
+                Directory.CreateDirectory(env.WebRootPath + "\\" + "LogFolder");
+            }
+                
+            StreamWriter swLog;
+            string logPath = "";
+
+            string FileName = DateTime.Now.ToString("ddMMyyyy") + ".txt";
+
+            logPath = Path.Combine(env.WebRootPath + "\\" + "LogFolder", FileName);
+
+            if (!File.Exists(logPath))
+            {
+                swLog = new StreamWriter(logPath);
+            }
+            else
+            {
+                swLog = File.AppendText(logPath);
+            }
+
+            swLog.WriteLine("Log Entry");
+            swLog.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+            swLog.Close();
+        }
+    }
+}
