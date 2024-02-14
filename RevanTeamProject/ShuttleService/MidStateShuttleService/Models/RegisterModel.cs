8e406ea885a9ebdb09f9084d3fd0bbcb3f1aae51
hphoomyint <lynne4791@gmail.com>
2024-02-13 19:22:43 -0600

    RV-31 Setup database and adjusted RegisterModel according to ERD

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Models/RegisterModel.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/RegisterModel.cs
@@ -7,9 +7,10 @@ namespace MidStateShuttleService.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Student ID is required")]
+        [Required(ErrorMessage = "User ID is required")]
         [RegularExpression("^[0-9]{10}$", ErrorMessage = "Must be 10 digits")]
-        public long StudentId { get; set; } // Changed from int to long
+        //public long StudentId { get; set; } // Changed from int to long
+        public int UserId { get; set; }
 
         [Required(ErrorMessage = "First Name is required")]
         [RegularExpression("^[A-Za-z\\s]{2,}$", ErrorMessage = "Must contain only characters and be at least 2 characters long")]
@@ -27,13 +28,15 @@ namespace MidStateShuttleService.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Trip Type is required")]
-        public string TripType { get; set; }
+        public string TripType { get; set; }// This could be a dropdown in the UI linked to Types available
 
         [Required(ErrorMessage = "Pick Up Location is required")]
-        public string PickUpLocation { get; set; }
+        //public string PickUpLocation { get; set; }
+        public int PickLocationID { get; set; }
 
         [Required(ErrorMessage = "Drop Off Location is required")]
-        public string DropOffLocation { get; set; }
+        //public string DropOffLocation { get; set; }
+        public int DropOffLocationID { get; set; }
 
         [StringLength(300, ErrorMessage = "Need transportation cannot exceed 300 characters")]
         public string NeedTransportation { get; set; }
@@ -50,10 +53,12 @@ namespace MidStateShuttleService.Models
 
         // New Properties for the Friday request form fields
         [Required(ErrorMessage = "Arrival Time is required")]
-        public DateTime ArrivalTime { get; set; }
+        [DataType(DataType.Time)]
+        public TimeSpan ArrivalTime { get; set; }
 
         [Required(ErrorMessage = "Departure Time is required")]
-        public DateTime DepartureTime { get; set; }
+        [DataType(DataType.Time)]
+        public TimeSpan DepartureTime { get; set; }
 
         [StringLength(300, ErrorMessage = "Which Friday cannot exceed 300 characters")]
         public string WhichFriday { get; set; }
