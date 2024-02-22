ee4f654b12d9d7b7616f4dcbcc07513091c5dcda
AlexisJ731 <AlexisJacobson731@gmail.com>
2024-02-22 13:03:04 -0600

    Added the Navigation to the Student communicate form

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/CommunicateController.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/CommunicateController.cs
@@ -29,5 +29,29 @@ namespace MidStateShuttleService.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// Displays the view for the student's communication form
+        /// </summary>
+        /// <returns> The Student Communicate View </returns>
+        public IActionResult StudentCommunicate()
+        {
+            return View();
+        }
+
+        // When the form submits, this method will play out.
+        [HttpPost]
+        public IActionResult StudentCommunicate(CommuncateModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                // Retrieve passed in list of students from the database.
+
+                // Send the message to each person registered to the shutte.
+                return RedirectToAction("MessageSent");
+            }
+
+            return View(c);
+        }
     }
 }
