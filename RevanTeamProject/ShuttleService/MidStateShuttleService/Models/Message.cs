e302c760efa4340df93b2894c014f02fefe677cb
AlexisJ731 <AlexisJacobson731@gmail.com>
2024-02-27 13:21:39 -0600

    Added additional feilds to the student communicate

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Models/Message.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/Message.cs
@@ -7,7 +7,15 @@ namespace MidStateShuttleService.Models
         public int id { get; set; }
 
         [Required(ErrorMessage = "A message is required.")]
+        [StringLength(160, ErrorMessage = "This field must not be more than 50 characters.")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "A message is required.")]
         [StringLength(160, ErrorMessage = "This field must not be more than 160 characters.")]
         public string message { get; set; }
+
+        public bool responseRequired { get; set; }
+
+        public string contactInfo { get; set; }
     }
 }
