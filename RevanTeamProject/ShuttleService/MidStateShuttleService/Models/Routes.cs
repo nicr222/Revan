642919e1002efe16ee12ade49cab56f6bb5ee01b
Nicolas Ross <15972995@mstc.edu>
2024-02-28 15:56:45 -0600

    RV-43 Added seperate css from what was being used before. and updated routes.cs modal and the html.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Models/Routes.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/Routes.cs
@@ -1,4 +1,7 @@
-﻿namespace MidStateShuttleService.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MidStateShuttleService.Models
 {
     public class Routes
     {
@@ -6,16 +9,31 @@
         public int RouteID { get; set; }
 
         // Route details
+
+        [Display(Name = "Pick Up Location")]
+        [Required(ErrorMessage = "Please select a pick-up location.")]
         public int PickUpLocationID { get; set; }
 
-        public Location PickLocation { get; set; }
+        [Display(Name = "Drop Off Location")]
+        [Required(ErrorMessage = "Please select a drop-off location.")]
         public int DropOffLocationID { get; set; }
 
+        public Location PickLocation { get; set; }
         public Location DropOffLocation { get; set; }
+
+        [Display(Name = "Pick Up Time")]
+        [Required(ErrorMessage = "Please enter a valid pick-up time.")]
+        [DataType(DataType.Time)]
+        [RegularExpression("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Please enter a valid time.")]
         public TimeSpan PickUpTime { get; set; }
+
+        [Display(Name = "Drop Off Time")]
+        [Required(ErrorMessage = "Please enter a valid drop-off time.")]
+        [DataType(DataType.Time)]
+        [RegularExpression("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Please enter a valid time.")]
         public TimeSpan DropOffTime { get; set; }
 
+        [Display(Name = "Additional Details")]
         public string AdditionalDetails { get; set; }
     }
-
 }
