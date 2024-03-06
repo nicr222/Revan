c88f8cdc653dcc5a844b2f55b3fd3c1a5ca033b6
Nicolas Ross <15972995@mstc.edu>
2024-03-06 16:58:23 -0600

    RV-36 finishing th last of the prep for the building of the tables to the dashboard.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Models/AllModels.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Models/AllModels.cs
@@ -2,5 +2,16 @@
 {
     public class AllModels
     {
+        public CommuncateModel Communcate { get; set; }
+
+        public Location Location { get; set; }
+
+        public Message Message { get; set; }
+
+        public RegisterModel Register { get; set; }
+
+        public ReservationModel Reservation { get; set; }
+
+        public Route Route { get; set; }
     }
 }
