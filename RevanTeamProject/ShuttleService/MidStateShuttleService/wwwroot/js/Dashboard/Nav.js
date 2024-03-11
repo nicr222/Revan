c14a81cae31fdda4d4bf94cb83965325531844e0
Nicolas Ross <15972995@mstc.edu>
2024-03-11 12:24:47 -0500

    RV-36 Added Nav.js file which is a simple nav toggle

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/Nav.js
@@ -0,0 +1,17 @@
+﻿let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
+// add hovered class in selected list item
+let list = document.querySelectorAll('.navigation li');
+function activatelink() {
+    list.forEach((item) =>
+        item.classList.remove('hovered'));
+    this.classList.add('hovered');
+}
+list.forEach((item) =>
+    item.addEventListener('mouseover', activatelink));
