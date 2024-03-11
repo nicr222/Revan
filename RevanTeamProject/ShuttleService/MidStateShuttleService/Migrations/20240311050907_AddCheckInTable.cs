5c30627d041bb778bb42fe128e5c687f21107f21
Nathan Brock <17134831@mstc.edu>
2024-03-11 00:09:42 -0500

    Add Check in Table

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Migrations/20240311050907_AddCheckInTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MidStateShuttleService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheckInTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
