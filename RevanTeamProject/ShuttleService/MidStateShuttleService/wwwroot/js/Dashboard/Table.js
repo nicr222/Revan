1d99e7e201cc6c9060ff22201fa7900a96dbf6f5
Nicolas Ross <15972995@mstc.edu>
2024-03-11 13:05:52 -0500

    RV-35 updated buttons to go to create for route and location. I also updated the tables .js to hid tables.

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/Table.js
@@ -0,0 +1,43 @@
+﻿$(function () {
+    // Function to hide all tables
+    function hideAllTables() {
+        $('.recentItem').hide();
+    }
+
+    // Initial setup to show all tables
+    hideAllTables();
+
+    // Click event for the Dashboard button
+    $('#order').click(function () {
+        hideAllTables();
+        return false;
+    });
+
+    // Click event for the Routes button
+    $('#employee').click(function () {
+        hideAllTables();
+        $('.recentItem.route').show();
+        return false;
+    });
+
+    // Click event for the Check Ins button
+    $('#product').click(function () {
+        hideAllTables();
+        $('.recentItem.recentFeedback').show();
+        return false;
+    });
+
+    // Click event for the Locations button
+    $('#department').click(function () {
+        hideAllTables();
+        $('.recentItem.location').show();
+        return false;
+    });
+
+    // Click event for the Messages button
+    $('#messages').click(function () {
+        hideAllTables();
+        $('.recentItem.recentFeedback').show(); // You can adjust this based on your actual structure
+        return false;
+    });
+});
