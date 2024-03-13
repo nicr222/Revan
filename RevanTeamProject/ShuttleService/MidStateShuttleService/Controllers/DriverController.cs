bca0068d8c5dc631f904c3414759567625bb7509
Nicolas Ross <15972995@mstc.edu>
2024-03-13 13:21:47 -0500

    RV-38 Updated the Controller to propperly post the creation of a driver to the DB.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/DriverController.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Controllers/DriverController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MidStateShuttleService.Models.Data;
+using System.Data;
 
 namespace MidStateShuttleService.Controllers
 {
     public class DriverController : Controller
     {
+
+        private readonly string connectionString;
+        private readonly ILogger<DriverController> _logger;
+
+        public DriverController(IConfiguration configuration, ILogger<DriverController> logger)
+        {
+            this.connectionString = configuration.GetConnectionString("DefaultConnection");
+            _logger = logger;
+        }
+
         // GET: DriverController
         public ActionResult Index()
         {
@@ -26,17 +39,64 @@ namespace MidStateShuttleService.Controllers
         // POST: DriverController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Driver driver)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                return View(driver);
             }
-            catch
+            else
             {
-                return View();
+
+
+                TempData["SuccessMessage"] = "The driver has been successfully created!";
+
             }
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string sql = "INSERT INTO [Driver] (Name, PhoneNumb, Email, IsActive) VALUES (@Name, @PhoneNumb, @Email, 1)";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+
+                    // Adding parameters
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@Name",
+                        Value = driver.Name,
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 100
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@PhoneNumb",
+                        Value = driver.PhoneNumb,
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 20
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@Email",
+                        Value = driver.Email,
+                        SqlDbType = SqlDbType.NVarChar,
+                        Size = 50
+                    };
+                    command.Parameters.Add(parameter);
+
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+            return RedirectToAction("Index", "Dashboard"); // Assuming "Home" is the controller where you want to redirect
         }
+        
 
         // GET: DriverController/Edit/5
         public ActionResult Edit(int id)
