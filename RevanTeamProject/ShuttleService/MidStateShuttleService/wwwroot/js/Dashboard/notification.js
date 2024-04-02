de8bd4e3b7c789ccc49798b19dddaf85e36a48f7
hphoomyint <lynne4791@gmail.com>
2024-04-02 09:17:57 -0500

    RV-60 Implemented notification function

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/notification.js
@@ -0,0 +1,18 @@
+﻿function toggleNotifications(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+
+    // Check if the dropdown menu is currently displayed
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+        // Hide any already open dropdowns
+        var dropdowns = document.getElementsByClassName('notifications');
+        for (var i = 0; i < dropdowns.length; i++) {
+            dropdowns[i].style.display = 'none';
+        }
+
+        // Show the relevant dropdown
+        dropdown.style.display = 'block';
+    } else {
+        // Hide the dropdown
+        dropdown.style.display = 'none';
+    }
+}
