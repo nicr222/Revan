d9670faba2e200f8547ff329d318bb1803a381fc
hphoomyint <lynne4791@gmail.com>
2024-04-03 09:07:58 -0500

    RV-60 Function toggle dropdown menu for notifications.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/notification.js
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/notification.js
@@ -1,18 +1,10 @@
 ﻿function toggleNotifications(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
+    var $dropdown = $('#' + dropdownId);
 
-    // Check if the dropdown menu is currently displayed
-    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-        // Hide any already open dropdowns
-        var dropdowns = document.getElementsByClassName('notifications');
-        for (var i = 0; i < dropdowns.length; i++) {
-            dropdowns[i].style.display = 'none';
-        }
+    // Hide any already open dropdowns
+    $('.dropdown-menu.notifications').not($dropdown).hide();
 
-        // Show the relevant dropdown
-        dropdown.style.display = 'block';
-    } else {
-        // Hide the dropdown
-        dropdown.style.display = 'none';
-    }
+    // Toggle the display of the dropdown
+    $dropdown.toggle();
 }
+
