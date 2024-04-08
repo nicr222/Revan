7898c9604f7cf8527d60edc711cafd6e93e46f84
AlexisJ731 <AlexisJacobson731@gmail.com>
2024-04-08 11:45:37 -0500

    Hosted to Azure

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
@@ -15,7 +15,7 @@ namespace MidStateShuttleService
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var appConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var appConnectionString = builder.Configuration.GetConnectionString("Connection") ?? throw new InvalidOperationException("Connection string 'Connection' not found.");
 
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(appConnectionString));
 
@@ -29,14 +29,6 @@ namespace MidStateShuttleService
             // Add services to the container.
             builder.Services.AddRazorPages();
 
-            // Add session services to the container
-            builder.Services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(30); // You can adjust the timeout as needed
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -49,11 +41,8 @@ namespace MidStateShuttleService
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            app.UseSession();
-
             app.UseRouting();
-            
+
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapRazorPages();
