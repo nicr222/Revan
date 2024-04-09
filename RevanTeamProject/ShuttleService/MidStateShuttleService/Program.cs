8acd49f37a49a17a10d682bba6a49c9fb57cc3ff
Nicolas Ross <15972995@mstc.edu>
2024-04-09 13:20:17 -0500

    RV-55 updated layout. I am going to send this to peer, the toggling wouldnt work because of the firstdaytoride data type. Will be bring that up with the team.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
@@ -29,6 +29,16 @@ namespace MidStateShuttleService
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            // Add session services to the container
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // You can adjust the timeout as needed
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -41,6 +51,9 @@ namespace MidStateShuttleService
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseSession();
+
             app.UseRouting();
             
             app.UseAuthentication();
