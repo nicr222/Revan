19bf982ab4be07b3bb09c84a89aa874457515b54
hphoomyint <lynne4791@gmail.com>
2024-04-09 13:35:55 -0500

    RV-14 Implemented star rating function

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Feedback/starRating.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Function to update stars
+    const updateStars = (value) => {
+        for (let i = 1; i <= 5; i++) {
+            const star = document.getElementById('star-' + i);
+            star.checked = i <= value;
+        }
+    };
+
+    // Add change event listeners to the star inputs
+    const stars = document.querySelectorAll('.star-rating input');
+    stars.forEach(star => {
+        star.addEventListener('change', function () {
+            updateStars(this.value);
+        });
+    });
+});
