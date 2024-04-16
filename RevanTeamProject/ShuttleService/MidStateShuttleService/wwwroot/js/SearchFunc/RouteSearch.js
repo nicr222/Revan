f1aca76aace500cb3c5a3eedf16c5a36821a4504
Nicolas Ross <15972995@mstc.edu>
2024-04-16 10:21:58 -0500

    RV-72 added search bar to tables. and added route and shuttle search js files

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/SearchFunc/RouteSearch.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('searchInput').addEventListener('input', function () {
+    var searchValue = this.value.toLowerCase();
+    var routeRows = document.getElementsByClassName('routeRow');
+
+    for (var i = 0; i < routeRows.length; i++) {
+        var pickupLocation = routeRows[i].getElementsByTagName('td')[1].innerText.toLowerCase();
+        var dropoffLocation = routeRows[i].getElementsByTagName('td')[2].innerText.toLowerCase();
+
+        if (pickupLocation.includes(searchValue) || dropoffLocation.includes(searchValue)) {
+            routeRows[i].style.display = '';
+        } else {
+            routeRows[i].style.display = 'none';
+        }
+    }
+});
