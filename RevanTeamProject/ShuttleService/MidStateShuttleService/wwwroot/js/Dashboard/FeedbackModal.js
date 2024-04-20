64a4d34c5e2035146b8fbf6a1c5d35626c243e07
hphoomyint <lynne4791@gmail.com>
2024-04-20 17:04:56 -0500

    RV-88 Created feedbackTabl.cshtml and Linked feedbackTable.cshtml to the nav bar

---
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/FeedbackModal.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('.viewButton').click(function () {
+        // Retrieve the feedback information from the row
+        var customerName = $(this).closest('tr').find('td:eq(0)').text();
+        var comment = $(this).closest('tr').find('td:eq(1)').text();
+        var rating = $(this).closest('tr').find('td:eq(2)').text();
+        var dateSubmitted = $(this).closest('tr').find('td:eq(3)').text();
+
+        // Update modal content with the data
+        $('#feedbackDetailsContent').html(
+            '<div>' +
+            '<p><strong>Customer Name:</strong> ' + customerName + '</p>' +
+            '<p><strong>Comment:</strong> ' + comment + '</p>' + // Displaying the full comment
+            '<p><strong>Rating:</strong> ' + rating + '</p>' +
+            '<p><strong>Date Submitted:</strong> ' + dateSubmitted + '</p>' +
+            '</div>'
+        );
+
+        // Open the modal
+        $('#feedbackDetailsModal').modal('show');
+    });
+});
