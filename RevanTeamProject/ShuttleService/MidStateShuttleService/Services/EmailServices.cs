367b403822a10050a6dd9e04e015c1c46e416290
AlexisJ731 <AlexisJacobson731@gmail.com>
2024-04-23 11:51:26 -0500

    Swapped send to sendAsync which should speed up the mass email sending

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Services/EmailServices.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Services/EmailServices.cs
@@ -24,7 +24,7 @@ namespace MidStateShuttleService.Services
             };
 
             // Send the email
-            client.Send(message);
+            client.SendMailAsync(message);
         }
     }
 }
