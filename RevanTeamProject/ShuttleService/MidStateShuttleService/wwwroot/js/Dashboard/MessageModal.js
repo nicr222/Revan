1f21d0961cff37f6a00fdf674b569025ef8c15e6
Nicolas Ross <15972995@mstc.edu>
2024-04-26 12:54:02 -0500

    Fixed the issue.

--- RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/MessageModal.js
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/MessageModal.js
@@ -1,14 +1,30 @@
 ﻿$(document).ready(function () {
-    // Event listener for the "View" link
     $('.viewButton').click(function () {
-        // Retrieve message details from data attributes
-        var messageId = $(this).attr('asp-route-id');
+        var name = $(this).closest('tr').find('td:eq(0)').text();
+        var message = $(this).closest('tr').find('td:eq(1)').text();
+        var responseRequired = $(this).closest('tr').find('td:eq(2)').text();
+        var contactInfo = $(this).closest('tr').find('td:eq(3)').text();
+
+        // Retrieve the full message content from the data attribute
         var fullMessage = $(this).data('full-message');
 
-        // Populate modal with message details
-        $('#messageDetailsContent').html('<strong>ID:</strong> ' + messageId + '<br><strong>Message:</strong> ' + fullMessage);
+        console.log('Name: ' + name);
+        console.log('Message: ' + message);
+        console.log('Response Required: ' + responseRequired);
+        console.log('Contact Info: ' + contactInfo);
+        console.log('Full Message: ' + fullMessage);
 
-        // Show the modal
+        // Update modal content with the data
+        $('#messageDetailsContent').html(
+            '<div>' +
+            '<p><strong>Name:</strong> ' + name + '</p>' +
+            '<p><strong>Message:</strong> ' + fullMessage + '</p>' + // Displaying the full message
+            '<p><strong>Response Required:</strong> ' + responseRequired + '</p>' +
+            '<p><strong>Contact Info:</strong> ' + contactInfo + '</p>' +
+            '</div>'
+        );
+
+        // Open the modal
         $('#messageDetailsModal').modal('show');
     });
 });
