7cfeced77e10579a3be46dd5c4726e66b1d1cc43
Nicolas Ross <15972995@mstc.edu>
2024-04-26 12:46:58 -0500

    Noticed that when someone merged they didn't handle merge conflicts or make sure they weren't deleting work that was done. Need to go and find that for message table

--- RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/MessageModal.js
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/wwwroot/js/Dashboard/MessageModal.js
@@ -1,30 +1,14 @@
 ﻿$(document).ready(function () {
+    // Event listener for the "View" link
     $('.viewButton').click(function () {
-        var name = $(this).closest('tr').find('td:eq(0)').text();
-        var message = $(this).closest('tr').find('td:eq(1)').text();
-        var responseRequired = $(this).closest('tr').find('td:eq(2)').text();
-        var contactInfo = $(this).closest('tr').find('td:eq(3)').text();
-
-        // Retrieve the full message content from the data attribute
+        // Retrieve message details from data attributes
+        var messageId = $(this).attr('asp-route-id');
         var fullMessage = $(this).data('full-message');
 
-        console.log('Name: ' + name);
-        console.log('Message: ' + message);
-        console.log('Response Required: ' + responseRequired);
-        console.log('Contact Info: ' + contactInfo);
-        console.log('Full Message: ' + fullMessage);
+        // Populate modal with message details
+        $('#messageDetailsContent').html('<strong>ID:</strong> ' + messageId + '<br><strong>Message:</strong> ' + fullMessage);
 
-        // Update modal content with the data
-        $('#messageDetailsContent').html(
-            '<div>' +
-            '<p><strong>Name:</strong> ' + name + '</p>' +
-            '<p><strong>Message:</strong> ' + fullMessage + '</p>' + // Displaying the full message
-            '<p><strong>Response Required:</strong> ' + responseRequired + '</p>' +
-            '<p><strong>Contact Info:</strong> ' + contactInfo + '</p>' +
-            '</div>'
-        );
-
-        // Open the modal
+        // Show the modal
         $('#messageDetailsModal').modal('show');
     });
 });
